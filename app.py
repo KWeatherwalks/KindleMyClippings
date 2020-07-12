a5053ca19ec3a96006876963d5f3429141268a79
Kevin Weatherwalks <kevin.weatherwalks@gmail.com>
2020-07-11 23:33:02 -0400

    created user menu to get input from user

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 import database
 
+MENU_PROMPT = """-- Kindle My Clippings Notes App -- 
+
+Please choose one of these options:
+
+1) Upload a new My Clippings.txt file
+2) See all notes
+3) Find notes by author
+4) Find notes by title
+5) Exit
+
+"""
+
 
 def menu():
     connection = database.connect()
     database.create_tables(connection)
+
+    while (user_input := input(MENU_PROMPT)) != "5":
+        print(user_input)
+
+menu()
--- database.py
+++ database.py
@@ -1,7 +1,10 @@
 import sqlite3
 
-CREATE_NOTES_TABLE = "CREATE TABLE notes (id INTEGER PRIMARY KEY, title TEXT, author TEXT, note TEXT, page INTEGER, location TEXT, date DATE);"
-INSERT_NOTE = "INSERT INTO notes (title, author, note, page, location, date) VALUES (?,?,?,?,?,?);"
+CREATE_NOTES_TABLE = "CREATE TABLE notes (id INTEGER PRIMARY KEY, title TEXT, edition TEXT author TEXT, note TEXT, page INTEGER, location TEXT, date DATE);"
+INSERT_NOTE = "INSERT INTO notes (title, edition, author, note, page, location, date) VALUES (?,?,?,?,?,?,?);"
+GET_ALL_NOTES = "SELECT * FROM notes;"
+GET_ALL_NOTES_BY_AUTHOR = "SELECT * FROM notes WHERE author = ?;"
+GET_ALL_NOTES_BY_TITLE = "SELECT * FROM notes WHERE title = ?;"
 
 
 def connect():
@@ -13,5 +16,23 @@ def create_tables(connection):
         connection.execute(CREATE_NOTES_TABLE)
 
 
-def add_note(connection, title, author, note, page, location, date):
-    connection.execute(INSERT_NOTE, (title, author, note, page, location, date))
+def add_note(connection, title, edition, author, note, page, location, date):
+    with connection:
+        connection.execute(
+            INSERT_NOTE, (title, edition, author, note, page, location, date)
+        )
+
+
+def get_all_notes(connection):
+    with connection:
+        return connection.execute(GET_ALL_NOTES).fetchall()
+
+
+def get_notes_by_author(connection, author):
+    with connection:
+        return connection.execute(GET_ALL_NOTES_BY_AUTHOR, (author,)).fetchall()
+
+
+def get_notes_by_title(connection, title):
+    with connection:
+        return connection.execute(GET_ALL_NOTES_BY_TITLE, (title,)).fetchall()
