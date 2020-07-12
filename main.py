1fde83eed89037b63e63a7c7a06658d1af0c3030
Kevin Weatherwalks <kevin.weatherwalks@gmail.com>
2020-07-11 20:08:33 -0400

    improved regex for title author to include collection/edition data

--- main.py
+++ main.py
@@ -2,26 +2,27 @@ import re
 
 import numpy as np
 import pandas as pd
-from unidecode import unidecode
+
+# from unidecode import unidecode
 
 
 def get_title_author(line):
     """Takes a string and returns a tuple containing the title and author
         if they match the title author format"""
     try:
-        title_author = re.split(title_author_pattern, line)
+        title_author = re.search(title_author_pattern, line)
     except ValueError:
         print("Value ERROR", title_author)
     except UnicodeError:
         print("Unicode ERROR")
 
-    if title_author:  # check that pattern was matched with line
-        # if so, we are at the beginning of the entry
-        title = title_author[1]
-        author = title_author[2][1:-1]
-        return title, author
-    else:
-        return None, None
+    title = title_author.group(1)
+    author = title_author.group(3)
+    collection = title_author.group(2)
+    if collection:
+        collection = collection.strip("()")
+
+    return title, author, collection
 
 
 def get_highlight_loc_date():
@@ -37,7 +38,7 @@ data_columns = ["title", "author", "note", "page", "location", "date_added"]
 df = pd.DataFrame(columns=data_columns)
 
 # <title> <(lastname, firstname)> pattern
-title_author_pattern = re.compile(r"(\w+.*)\s+(\(.+\))")
+title_author_pattern = re.compile(r"(\w+[^(]*) (\(.+\))?\s*\((.+)\)")
 
 with open("My Clippings.txt", "r", encoding="utf-8") as file:
     line = file.readline()
@@ -48,16 +49,14 @@ with open("My Clippings.txt", "r", encoding="utf-8") as file:
         # Line 1: get the title and author name
         line = file.readline()
         if line:
-            # print("Reading line 1: {0}".format(line))
-            title, author = get_title_author(line)
+            print("\nReading line 1: {0}".format(line), end="")
+            title, author, collection = get_title_author(line)
             line_count += 1
-        if title or author:
-            print("\nTitle:\t", title, "\nAuthor:\t", author)
 
         # Line 2: the highlight, location, date added phase
         line2 = file.readline()
         if line2:
-            # print("Reading line 2: {0}".format(line2))
+            print("Reading line 2: {0}".format(line2))
             line_count += 1
 
         # Line 3: blank
@@ -77,5 +76,7 @@ with open("My Clippings.txt", "r", encoding="utf-8") as file:
         if line5:
             # print("Reading line 5: {0}".format(line5))
             line_count += 1
+
+        print("Title:\t", title, "\nAuthor:\t", author, "\nEdition:\t", collection)
 
 print(f"{line_count} lines read")
